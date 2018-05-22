f21fd3d8fe8b7a7d4db41e0f97c335b2b6f67c6e
daviddell <parker@hrdl.io>
2018-05-22 12:13:11 -0400

    set up app.jsx file

---
+++ views/js/app.jsx
@@ -0,0 +1,92 @@
+/*This component bootstraps our entire React app. 
+It decides which component to show based on whether a user is authenticated or not. 
+We’ll start off with just its base, and later update it with more functionality.
+*/
+class App extends React.Component {
+  render() {
+    if (this.loggedIn) {
+      return (<LoggedIn />);
+    } else {
+      return (<Home />);
+    }
+  }
+}
+
+
+/*This component is shown to non logged-in users, along with a button which opens a Hosted lock screen where they can signup or login. 
+We’ll add this functionality later.
+*/
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
+          <h1>Jokeish</h1>
+          <p>A load of Dad jokes XD</p>
+          <p>Sign in to get access </p>
+          <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+//This component is displayed when a user is authenticated. It stores in its state an array of jokes which is populated when the component mounts.
+class LoggedIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: []
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="col-lg-12">
+          <br />
+          <span className="pull-right"><a onClick={this.logout}>Log out</a></span>
+          <h2>Jokeish</h2>
+          <p>Let's feed you with some funny Jokes!!!</p>
+          <div className="row">
+            {this.state.jokes.map(function(joke, i){
+              return (<Joke key={i} joke={joke} />);
+            })}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+//The Joke component will contain information about each item from the jokes response to be displayed.
+class Joke extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: ""
+    }
+    this.like = this.like.bind(this);
+  }
+  like() {
+    // ... we'll add this block later
+  }
+  render() {
+    return (
+      <div className="col-xs-4">
+        <div className="panel panel-default">
+          <div className="panel-heading">#{this.props.joke.id} <span className="pull-right">{this.state.liked}</span></div>
+          <div className="panel-body">
+            {this.props.joke.joke}
+          </div>
+          <div className="panel-footer">
+            {this.props.joke.likes} Likes &nbsp;
+            <a onClick={this.like} className="btn btn-default">
+              <span className="glyphicon glyphicon-thumbs-up"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
